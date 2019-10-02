a4ef412db7f7d59e630b92f8429c25afe0b03330
Rob <robert.andersen@ontariotechu.net>
2019-10-02 16:53:42 -0400

    Adding play functionallity

--- GroupAssignment1 - Engines/Assets/_Scripts/AddObject.cs
+++ GroupAssignment1 - Engines/Assets/_Scripts/AddObject.cs
@@ -7,12 +7,15 @@ public class AddObject : MonoBehaviour
     public KeyCode createKey;
     public KeyCode finalizeKey;
     public KeyCode selectKey;
+    public KeyCode playKey;
     public GameObject platform;
     public GameObject finishPlatform;
     int finishPlat = 0;
     int Selection = 0;
     bool keyPressed = false;
     float timer = 0;
+    public bool playerActive = false;
+
 
     IList<GameObject> platformCopy = new List<GameObject>();
     IList<float> platformCount = new List<float>();
@@ -26,54 +29,47 @@ public class AddObject : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        // if (Input.GetKeyDown(createKey))    
-        //{
-        //    keyPressed = true;
-        //    timer = Time.time;
-        //}
-        //else if (Input.GetKeyUp(createKey))
-        //{
-        //   keyPressed = false;
-        //resourceCollected = 0;
-        //}
-        if (Input.GetKeyDown(selectKey))
+        if (Input.GetKeyDown(playKey))
         {
-            Selection += 1;
-            Debug.Log(Selection);
+             playerActive = true;
         }
-        //Picks up resource after 1 second of holding the key down (Key is public, it is et in Unity)
-        //Also sends a bool to onTriggerStay
-        if (Selection == 0)
+        else
         {
-            if (Input.GetKeyDown(createKey))
+            if (Input.GetKeyDown(selectKey))
             {
-                platformCopy.Add(Instantiate(platform));
-                platformCount.Add(0);
-
-                platformCopy[platformCopy.Count - 1].transform.position = transform.position;
-                //direction.Add(transform.forward);
-                //platformCopy[platformCop.Count - 1].transform.position = transform.position;
+                Selection += 1;
+                Debug.Log(Selection);
             }
-        }
 
-        else if (Selection >= 1)
-        {
-            if (Input.GetKeyDown(createKey))
+            if (Selection == 0)
             {
-                if (finishPlat < 1)
+                if (Input.GetKeyDown(createKey))
                 {
-                    platformCopy.Add(Instantiate(finishPlatform));
+                    platformCopy.Add(Instantiate(platform));
                     platformCount.Add(0);
+
                     platformCopy[platformCopy.Count - 1].transform.position = transform.position;
-                    finishPlat += 1;
                 }
-                else
-                    Debug.Log("Platform already placed");
-                //direction.Add(transform.forward);
-                //platformCopy[platformCop.Count - 1].transform.position = transform.position;
             }
 
+            else if (Selection >= 1)
+            {
+                if (Input.GetKeyDown(createKey))
+                {
+                    if (finishPlat < 1)
+                    {
+                        platformCopy.Add(Instantiate(finishPlatform));
+                        platformCount.Add(0);
+                        platformCopy[platformCopy.Count - 1].transform.position = transform.position;
+                        finishPlat += 1;
+                    }
+                    else
+                        Debug.Log("Platform already placed");
+                }
+
+            }
         }
+
 
     }
 }
